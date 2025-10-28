95614a46ea5cc6e2b42a4668c322b4f2b1554cfe
Bkn <beckan0011@gmail.com>
2025-10-28 16:02:57 -0500

    First

---
+++ src/test/java/com/tpdteam3/back_examen/BackExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpdteam3.back_examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
